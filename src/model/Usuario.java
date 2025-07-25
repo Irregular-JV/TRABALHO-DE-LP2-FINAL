b894a5791bde881d42a4b71e84b330ca97854590
Kadu Lobato <lobatokadu2005@gmail.com>
2025-07-24 23:52:58 -0300

    Implementação da classe Usuário relacionando-a com o Banco de Dados

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,5 +1,53 @@
 package model;
 
 public class Usuario {
+    private int id;
+    private String username;
+    private String senha;
+    private String nivelAcesso;
+
+    // Construtores
     
+    public Usuario(String username, String senha, String nivelAcesso) {
+        this.username = username;
+        this.senha = senha;
+        this.nivelAcesso = nivelAcesso;
+    }
+
+    public Usuario(int id, String username, String senha, String nivelAcesso) {
+        this.id = id;
+        this.username = username;
+        this.senha = senha;
+        this.nivelAcesso = nivelAcesso;
+    }
+    
+    // Getters e Setters
+
+    public int getId(){
+        return this.id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getUsername(){
+        return this.username;
+    }
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public String getSenha(){
+        return this.senha;
+    }
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+
+     public String getNivelAcesso(){
+        return this.nivelAcesso;
+    }
+    public void setNivelAcesso(String nivelAcesso){
+        this.nivelAcesso = nivelAcesso;
+    }
 }
