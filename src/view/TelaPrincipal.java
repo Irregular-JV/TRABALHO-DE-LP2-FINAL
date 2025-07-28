332b52a395d6a89042bfdc06cbb1754822f60207
Kadu Lobato <157190522+kaducl@users.noreply.github.com>
2025-07-28 17:24:15 -0300

    fix: botões da home agora com funcionalidade

--- src/view/TelaPrincipal.java
+++ src/view/TelaPrincipal.java
@@ -8,7 +8,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import controller.EntrarController;
 import model.UsuarioDAO;
 
 import java.awt.BorderLayout;
@@ -178,9 +177,19 @@ public class TelaPrincipal extends JFrame{
 
     public void registrarPaineis() {
         painelReservas = new PainelReservas();
-        PainelHome home = new PainelHome(nomeUsuario, idUsuario, () -> {
-            new TelaNovaReserva(this, idUsuario, () -> painelReservas.atualizarReservas());
-        });
+    
+        CardLayout layout = (CardLayout) painelPrincipal.getLayout();
+        Map<String, Runnable> acoesHome = new HashMap<>();
+
+        acoesHome.put("Reservas", () -> layout.show(painelPrincipal, "Reservas"));
+        acoesHome.put("Gerenciar Espaços", () -> layout.show(painelPrincipal, "Gerenciar Espaços"));
+        acoesHome.put("Usuários", () -> layout.show(painelPrincipal, "Usuários"));
+
+        painelReservas = new PainelReservas();
+        PainelHome home = new PainelHome(nomeUsuario, idUsuario, 
+            () -> new TelaNovaReserva(this, idUsuario, () -> painelReservas.atualizarReservas()), 
+            acoesHome // Passa o mapa de ações
+        );
       
         PainelUsuarios painelUsuarios = new PainelUsuarios(this.nomeUsuario);
         UsuarioDAO dao = new UsuarioDAO();
@@ -198,7 +207,7 @@ public class TelaPrincipal extends JFrame{
         for(Map.Entry<String, JPanel> entry: painelRegistradores.entrySet()) {
             painelPrincipal.add(entry.getValue(), entry.getKey());
         }
-        CardLayout layout = (CardLayout) painelPrincipal.getLayout();
-        layout.show(painelPrincipal, "Home"); // mostra o painel Home ao iniciar
+        
+        layout.show(painelPrincipal, "Home");
     }
 }
